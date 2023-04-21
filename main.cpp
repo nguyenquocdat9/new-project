d2e95b68442b5d7c4fc14c232fce33a7ba882af6
nguyenquocdat9 <124764098+nguyenquocdat9@users.noreply.github.com>
2023-04-21 11:41:29 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 void refreshScreen(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* background, Entity& Ship, Entity& asteroid1, Entity& asteroid2, Entity& asteroid3, Entity& asteroid4
-                   , Entity& Shield, bool ShieldUp, bool ShieldDisappear, bool ShieldGot);
+                   , Entity& Shield, bool ShieldUp, bool ShieldDisappear, bool ShieldGot, Entity& Buff, bool BuffUp, bool BuffDisappear, bool BuffGot);
 
 int main(int argc, char* argv[])
 {
@@ -27,6 +27,8 @@ int main(int argc, char* argv[])
     SDL_Texture* barrier = loadTexture("image/shield.png", renderer);
     SDL_Texture* spaceship = loadTexture("image/spaceship.png", renderer);
     SDL_Texture* spaceshipShield = loadTexture("image/spaceshipwithshield.png", renderer);
+    //buff
+    SDL_Texture* buff = loadTexture("image/slow.png", renderer);
     //menu
     SDL_Texture* GameOverScreen = loadTexture("image/gameover.jpg", renderer);
     //music
@@ -38,14 +40,18 @@ int main(int argc, char* argv[])
     int TimeSpawn = 20; // 20sec
     int DisappearTime = 25;//5sec
     int FadingTime = 5; // shield time 5sec
+    //slow buff
+    int TimeSpawnBuff = 40;//40 sec
+    int BuffDisappearTime = 45;//5 sec
 
     bool GameIsRunning = true;
-    bool QuitGame = true;
+    bool QuitGame = false;
+    bool MainMenuOn = true;
     SDL_Event e;
     srand((unsigned int) time(NULL));
     SDL_Texture* ShipPhoto;
 
-  while(QuitGame)
+  while(!QuitGame)
   {
     // 1, 3 spawn trai
     // 2, 4 spawn phai
@@ -69,8 +75,13 @@ int main(int argc, char* argv[])
     //game tempo
     int speed = 8;
     clock_t SpeedupTime = clock();
+    //buff
+    clock_t BuffStartTime = clock();
+    int xBuff= (rand() % 1400) + 1;
+    int yBuff = (rand() % 696) + 1;
     //ship
     ShipPhoto = spaceship;
+
 
 
     while(GameIsRunning)
@@ -91,6 +102,9 @@ int main(int argc, char* argv[])
         //shield related
         Entity *Shield = new Entity(xShield, yShield, barrier);
         Shield->type = "Shield";
+        //buff related
+        Entity *SlowBuff = new Entity(xBuff, yBuff, buff);
+        SlowBuff->type = "buff";
 
         // Nếu không có sự kiện gì thì tiếp tục trở về đầu vòng lặp
           while( SDL_PollEvent(&e) != 0 ){
@@ -144,10 +158,23 @@ int main(int argc, char* argv[])
             ShipPhoto = spaceship;
             Invulnerable = false;
         }
+        //buff
+        clock_t BuffSpawnElapsedTime = clock() - BuffStartTime;
 
+        if(ShieldTimer(BuffSpawnElapsedTime, TimeSpawnBuff) && ShieldTimer(BuffSpawnElapsedTime, BuffDisappearTime) == false)
+        {
+            if(CheckCollision(Xship, Yship, xBuff, yBuff))
+            {
+                xBuff= (rand() % 1400) + 1;
+                yBuff = (rand() % 696) + 1;
+                speed -= 3;
+                BuffStartTime = clock();
+            }
+        }
 
         refreshScreen(window, renderer, background, Ship, *asteroid1, *asteroid2, *asteroid3, *asteroid4, *Shield, ShieldTimer(SpawnElapsedTime, TimeSpawn)
-                      , ShieldTimer(DisappearElapsedTime, DisappearTime), ShieldObtained(Xship, Yship, xShield, yShield));
+                      , ShieldTimer(DisappearElapsedTime, DisappearTime), ShieldObtained(Xship, Yship, xShield, yShield), *SlowBuff, ShieldTimer(BuffSpawnElapsedTime, TimeSpawnBuff)
+                      , ShieldTimer(BuffSpawnElapsedTime, BuffDisappearTime), CheckCollision(Xship, Yship, xBuff, yBuff));
         //if shield not obtained after DisappearTime reset StartTime and shield location
         if(ShieldTimer(DisappearElapsedTime, DisappearTime))
         {
@@ -155,12 +182,20 @@ int main(int argc, char* argv[])
             yShield = (rand() % 696) + 1;
             StartTime = clock();
         }
+        //buff
+        if(ShieldTimer(BuffSpawnElapsedTime, BuffDisappearTime))
+        {
+            xBuff= (rand() % 1400) + 1;
+            yBuff = (rand() % 696) + 1;
+            BuffStartTime = clock();
+        }
         //delete all dynamic variable
         delete asteroid1;
         delete asteroid2;
         delete asteroid3;
         delete asteroid4;
         delete Shield;
+        delete SlowBuff;
 
         // object
         clock_t SpeedUpElapsed = clock() - SpeedupTime;
@@ -169,6 +204,7 @@ int main(int argc, char* argv[])
             speed += 2;
             SpeedupTime = clock();
         }
+        //change asteroid position
         x1 += speed;
         x2 -= speed;
         x3 += speed;
@@ -193,6 +229,7 @@ int main(int argc, char* argv[])
             x4 = (rand() % 600) + 1501;
             y4 = (rand() % 696) + 1;
         }
+        //check collision
         if(CheckCollision(Xship, Yship, x1, y1))
         {
             if(Invulnerable == false)
@@ -230,26 +267,28 @@ int main(int argc, char* argv[])
         int delta = SDL_GetTicks() - start;
         if(delta < DELTA) SDL_Delay(DELTA - delta);
     }
+
     SDL_RenderClear(renderer);
+    //Game Over Screen
     SDL_RenderCopy(renderer, GameOverScreen, NULL, NULL);
     SDL_RenderPresent(renderer);
     while(SDL_PollEvent(&e) != 0)
     {
         if(e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT)
         {
-           if(e.button.x >= 140 && e.button.x <= 430 && e.button.y >= 615 && e.button.y <= 735) GameIsRunning = true; // play again
-           else if(e.button.x >= 1030 && e.button.x <= 1285 && e.button.y >= 615 && e.button.y <= 735) QuitGame = false; // quit game
+           if(e.button.x >= 609 && e.button.x <= 895 && e.button.y >= 490 && e.button.y <= 575) GameIsRunning = true; // play again
+           else if(e.button.x >= 609 && e.button.x <= 895 && e.button.y >= 691 && e.button.y <= 772) QuitGame = true; // quit game
         }
     }
   }
-    // button NO 1030 -> 1290 ; 615 -> 750
-    // button YES 140 -> 435 ; 615 -> 750
+    // button QUIT GAME 609 -> 895 ; 691 -> 772
+    // button PLAY AGAIN 609 -> 895 ; 490 -> 575
     quitSDL(window, renderer);
     return 0;
 }
 
 void refreshScreen(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* background, Entity& Ship, Entity& asteroid1, Entity& asteroid2, Entity& asteroid3, Entity& asteroid4
-                    , Entity& Shield, bool ShieldUp, bool ShieldDisappear, bool ShieldGot)
+                    , Entity& Shield, bool ShieldUp, bool ShieldDisappear, bool ShieldGot, Entity& Buff, bool BuffUp, bool BuffDisappear, bool BuffGot)
 {
     SDL_RenderClear(renderer);
 
@@ -259,6 +298,9 @@ void refreshScreen(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* back
 
     if(ShieldUp && ShieldDisappear == false && ShieldGot == false) render(Shield, renderer);
     // render shield
+
+    if(BuffUp && BuffDisappear == false && BuffGot == false) render(Buff, renderer);
+    //render buff
 
     render(asteroid1,renderer);
     render(asteroid2,renderer);
